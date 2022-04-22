4763693511b4e234b7ec098bc04596f36ecbcab7
saymon_says <serdiukovsam@gmail.com>
2022-04-22 12:40:50 +0300

    Initial commit

---
+++ src/test/java/saymon/testwork/LED/LedApplicationTests.java
@@ -0,0 +1,13 @@
+package saymon.testwork.LED;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
